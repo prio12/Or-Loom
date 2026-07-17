4ef5db1c06b3f414c6b9b5292052d86cdc747ecd
prio12 <maksudurprio.mp@gmail.com>
2026-07-17 16:50:16 +0600

    feat: whishlist love icon added in the product details page

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -4,6 +4,7 @@ import { use, useState } from "react";
 import { notFound } from "next/navigation";
 import Image from "next/image";
 import { motion } from "framer-motion";
+import { CiHeart } from "react-icons/ci";
 
 export default function ProductDetails({
   params,
@@ -17,12 +18,13 @@ export default function ProductDetails({
 
   const [selectedColor, setSelectedColor] = useState(product.colors[0]);
   const [selectedSize, setSelectedSize] = useState(product.sizes[0]);
+  const [isWishlisted, setIsWishlisted] = useState(false);
 
   return (
     <div className="bg-[#F7F5F2] px-4 md:px-10 py-10">
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
-        {/* LEFT: image */}
-        <div className="relative aspect-[3/4] overflow-hidden bg-white">
+        {/* Left: image */}
+        <div className="relative aspect-3/4 overflow-hidden bg-white">
           <motion.div
             whileHover={{ scale: 1.08 }}
             transition={{ duration: 0.5, ease: [0.22, 1, 0.36, 1] }}
@@ -37,7 +39,7 @@ export default function ProductDetails({
           </motion.div>
         </div>
 
-        {/* RIGHT: info */}
+        {/* Right: info */}
         <div className="flex flex-col gap-4">
           <p className="text-xs uppercase tracking-widest text-[#8A8578]">
             {product.category}
@@ -46,6 +48,20 @@ export default function ProductDetails({
           <h1 className="font-serif text-3xl">{product.name}</h1>
 
           <p className="text-lg text-[#B5532C]">${product.price}</p>
+          <button
+            onClick={() => setIsWishlisted(!isWishlisted)}
+            className="flex items-center gap-2 text-sm w-fit cursor-pointer"
+          >
+            <CiHeart
+              size={22}
+              className={
+                isWishlisted
+                  ? "text-[#B5532C] fill-[#B5532C]"
+                  : "text-[#1A1A1A]"
+              }
+            />
+            {isWishlisted ? "Saved" : "Add to Wishlist"}
+          </button>
 
           <p className="text-sm text-[#8A8578]">★ {product.rating}</p>
 
@@ -58,7 +74,7 @@ export default function ProductDetails({
                 <button
                   key={color}
                   onClick={() => setSelectedColor(color)}
-                  className={`px-3 py-1 text-sm rounded-full border ${
+                  className={`px-3 py-1 text-sm rounded-full border cursor-pointer ${
                     selectedColor === color
                       ? "bg-[#1A1A1A] text-white"
                       : "bg-white"
