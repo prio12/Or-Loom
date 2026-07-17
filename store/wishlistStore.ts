0090dd8b6d11f5a3cbf46bbd0be10905c9473854
prio12 <maksudurprio.mp@gmail.com>
2026-07-17 18:03:18 +0600

    feat: added zustand persist middleware

--- store/wishlistStore.ts
+++ store/wishlistStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface WishlistItem {
   id: number;
@@ -13,14 +14,19 @@ interface WishlistState {
   isWishlisted: (id: number) => boolean;
 }
 
-export const useWishlistStore = create<WishlistState>((set, get) => ({
-  items: [],
-  toggleItem: (item) =>
-    set((state) => {
-      const exists = state.items.find((i) => i.id === item.id);
-      return exists
-        ? { items: state.items.filter((i) => i.id !== item.id) }
-        : { items: [...state.items, item] };
+export const useWishlistStore = create<WishlistState>()(
+  persist(
+    (set, get) => ({
+      items: [],
+      toggleItem: (item) =>
+        set((state) => {
+          const exists = state.items.find((i) => i.id === item.id);
+          return exists
+            ? { items: state.items.filter((i) => i.id !== item.id) }
+            : { items: [...state.items, item] };
+        }),
+      isWishlisted: (id) => get().items.some((i) => i.id === id),
     }),
-  isWishlisted: (id) => get().items.some((i) => i.id === id),
-}));
+    { name: "wishlist-storage" },
+  ),
+);
